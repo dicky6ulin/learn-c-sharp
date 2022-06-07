333d67659f5bfe82244666ec50d0d501ae5108a7
Dicky Agus Setiawan <dickyagussetiawan@gmail.com>
2022-06-07 13:54:04 +0700

    [FITURE] add 02.take-Input-from-user

--- AppLearn/Program.cs
+++ AppLearn/Program.cs
@@ -10,19 +10,11 @@ namespace AppLearn
     {
         static void Main(string[] args)
         {
-            // variable adalah tempat untuk menyimpan nilai/data
-            // tipe data adalah jenis data
+            string nama;
+            Console.Write("Siapa nama anda : ");
+            nama = Console.ReadLine();
 
-            int a = 10;
-            int b = a + 10;
-            Console.WriteLine("A = " + a);
-            Console.WriteLine("B = " + b);
-
-            String nama = "Dicky Agus Setiawan";
-            Console.WriteLine(nama);
-
-            Boolean keadaan = true;
-            Console.WriteLine(keadaan);
+            Console.WriteLine("Nama anda adalah : " + nama);
         }
     }
 }
