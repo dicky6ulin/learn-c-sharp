843be1547a055e0e87d0cb76e149272dd9091fd5
Dicky Agus Setiawan <dickyagussetiawan@gmail.com>
2022-06-07 13:45:39 +0700

    [FITURE] add 01.variable-and-datatype

--- AppLearn/Program.cs
+++ AppLearn/Program.cs
@@ -10,7 +10,19 @@ namespace AppLearn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !!!");
+            // variable adalah tempat untuk menyimpan nilai/data
+            // tipe data adalah jenis data
+
+            int a = 10;
+            int b = a + 10;
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+
+            String nama = "Dicky Agus Setiawan";
+            Console.WriteLine(nama);
+
+            Boolean keadaan = true;
+            Console.WriteLine(keadaan);
         }
     }
 }
