79bc597f38dd018572a96841631ae9c27ae65b06
Dicky Agus Setiawan <dickyagussetiawan@gmail.com>
2022-06-07 15:05:18 +0700

    [FITURE] add 08.array

--- AppLearn/Program.cs
+++ AppLearn/Program.cs
@@ -10,30 +10,33 @@ namespace AppLearn
     {
         static void Main(string[] args)
         {
-            String username;
-            String password;
+            //int nilai1 = 10;
+            //int nilai2 = 20;
+            //int nilai3 = 30;
+            //int nilaI4 = 40;
 
-            Console.Write("Masukan username = ");
-            username = Console.ReadLine();
+            //int[] nilai = {nilai1, nilai2, nilai3, nilaI4};
+            //Console.WriteLine(nilai[0]);
 
-            if(username == "admin")
-            {
-                Console.Write("Masukan passworld = ");
-                password = Console.ReadLine();
+            // ==================================================
 
-                if (password == "admin")
-                {
-                    Console.WriteLine("Selamat datang admin");
-                } 
-                else
-                {
-                    Console.WriteLine("Password salah");
-                }
-            } 
-            else
-            {
-                Console.WriteLine("Username tidak ditemukan");
-            }
+            //int[] nilai = new int[2];
+            //nilai[0] = 10;
+            //nilai[1] = 20;
+            //nilai[2] = 30;
+
+            //Console.WriteLine(nilai[0]);
+
+            // ==================================================
+
+            String[] peliharaan = new String[2];
+            peliharaan[0] = "Ayam";
+            peliharaan[1] = "Kucing";
+            peliharaan[2] = "ikan";
+
+            Console.WriteLine(peliharaan[0]);
+
+
         }
     }
 }
