d20bc47b189f9d172b34de4f58d89480461fc72d
Dicky Agus Setiawan <dickyagussetiawan@gmail.com>
2022-06-14 11:56:14 +0700

    [FITURE] add 14.void

--- AppLearn/Program.cs
+++ AppLearn/Program.cs
@@ -8,17 +8,78 @@ namespace AppLearn
 {
     internal class Program
     {
+        // void ==> method atau fungction
+
+
+        //static void main(string[] args)
+        //{
+        //    tampilkan();
+
+        //    void tampilkan()
+        //    {
+        //        for( int i = 0; i < 5; i++)
+        //        {
+        //            console.writeline("hallo semuanya");
+        //        }
+        //    }
+        //}
+
+
+
+        //static void Main(string[] args)
+        //{
+        //    Tampilkan();
+        //}
+
+        //static void Tampilkan()
+        //{
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        Console.WriteLine("Hallo Semuanya");
+        //    }
+        //}
+
+
+
+        //static void Main(string[] args)
+        //{
+        //    Tambah(20,10);
+        //}
+
+        //static void Tambah(int a, int b)
+        //{
+        //    Console.WriteLine("Total : " + (a + b));
+        //}
+
+
         static void Main(string[] args)
         {
-            // < > == <= >=
-            if(5 < 10)
+            Login();
+        }
+
+        static void Login()
+        {
+            string username = "";
+            string password = "";
+
+            while (username != "admin" && password != "admin")
             {
-                Console.WriteLine("5 kurang dari 10");
-            } 
-            else
-            {
-                Console.WriteLine("false");
+                Console.Write("Username : ");
+                username = Console.ReadLine();
+                Console.Write("Password : ");
+                password = Console.ReadLine();
+
+                if (username == "admin" && password == "admin")
+                {
+                    Console.WriteLine("Selamat datang admin !");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Username or password salah");
+                }
             }
         }
+
     }
 }
