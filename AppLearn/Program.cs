fcf3b6b931346460c1da384ab792dc3ba5f16b78
Dicky Agus Setiawan <dickyagussetiawan@gmail.com>
2022-06-14 14:32:30 +0700

    [FITURE] add 15.fungsi-return

--- AppLearn/Program.cs
+++ AppLearn/Program.cs
@@ -6,80 +6,47 @@ using System.Threading.Tasks;
 
 namespace AppLearn
 {
-    internal class Program
+    class Program
     {
-        // void ==> method atau fungction
-
-
-        //static void main(string[] args)
-        //{
-        //    tampilkan();
-
-        //    void tampilkan()
-        //    {
-        //        for( int i = 0; i < 5; i++)
-        //        {
-        //            console.writeline("hallo semuanya");
-        //        }
-        //    }
-        //}
-
-
-
-        //static void Main(string[] args)
-        //{
-        //    Tampilkan();
-        //}
-
-        //static void Tampilkan()
-        //{
-        //    for (int i = 0; i < 5; i++)
-        //    {
-        //        Console.WriteLine("Hallo Semuanya");
-        //    }
-        //}
-
-
-
-        //static void Main(string[] args)
-        //{
-        //    Tambah(20,10);
-        //}
-
-        //static void Tambah(int a, int b)
-        //{
-        //    Console.WriteLine("Total : " + (a + b));
-        //}
-
-
         static void Main(string[] args)
         {
-            Login();
-        }
-
-        static void Login()
-        {
-            string username = "";
-            string password = "";
-
-            while (username != "admin" && password != "admin")
+            int tambah(int a, int b)
             {
-                Console.Write("Username : ");
-                username = Console.ReadLine();
-                Console.Write("Password : ");
-                password = Console.ReadLine();
-
-                if (username == "admin" && password == "admin")
-                {
-                    Console.WriteLine("Selamat datang admin !");
-                }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine("Username or password salah");
-                }
+                return a + b;
             }
-        }
+            int hasil = tambah(5, 5);
 
+            Console.WriteLine(hasil);
+
+
+
+            //bool apakahLulus(int nilai)
+            //{
+            //    if (nilai < 78)
+            //    {
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+
+            //Console.Write("Masukan Nilai : ");
+            //int nilaiFinal = Convert.ToInt32(Console.ReadLine());
+
+            //if (apakahLulus(nilaiFinal))
+            //{
+            //    Console.WriteLine("anda lulus !");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("anda tidak lulus");
+            //}
+
+            string nilaiFnal = (hasil < 78) ? "Anda lulus !" : "Anda tidak lulus";
+            Console.WriteLine(nilaiFnal);
+
+        }
     }
 }
